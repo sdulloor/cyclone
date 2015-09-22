4ce9c0790d506e66d997267264e88f582aea226f
Dulloor <subramanya.r.dulloor@intel.com>
2015-09-21 19:59:46 -0700

    Compile fixes.

--- libcyclone.hpp
+++ libcyclone.hpp
@@ -2,7 +2,7 @@
 #define _CYCLONE_HPP_
 
 int cyclone_is_leader();
-void cyclone_add_entry(const unsigned char *data, const int size);
+int cyclone_add_entry(unsigned char *data, const int size);
 int cyclone_req_complete();
 typedef void (*cyclone_callback_t)(const unsigned char *data, const int len);
 extern void cyclone_boot(char *config_path,
