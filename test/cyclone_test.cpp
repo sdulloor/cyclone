6ce13adba1ed0059abd8701bef230af65399c7a4
Dulloor <subramanya.r.dulloor@intel.com>
2015-10-06 09:51:23 -0700

    Use boost log for messages from test client.

--- test/cyclone_test.cpp
+++ test/cyclone_test.cpp
@@ -4,19 +4,22 @@
 #include<unistd.h>
 #include<libcyclone.hpp>
 #include "../core/clock.hpp"
+#include<boost/log/trivial.hpp>
 
 rtc_clock timer;
 
 void trace_pre_append(void *data, const int size)
 {
   if(size != 8) {
-    fprintf(stderr, "ERROR\n");
+    BOOST_LOG_TRIVIAL(fatal) << "SERVER: Incorrect record size";
+    exit(-1);
   }
   else {
     unsigned int elapsed_msecs = timer.current_time()/1000;
-    fprintf(stderr, "PRE_APPEND %d:%d\n",
-	    *(const unsigned int *)data,
-	    elapsed_msecs - *(const unsigned int *)((char *)data + 4));
+    BOOST_LOG_TRIVIAL(info) 
+      << "SERVER: PRE_APPEND "
+      << *(const unsigned int *)data << " "
+      << (elapsed_msecs - *(const unsigned int *)((char *)data + 4));
   }
   __sync_synchronize();
 }
@@ -26,13 +29,15 @@ void trace_pre_append(void *data, const int size)
 void trace_post_append(void *data, const int size)
 {
   if(size != 8) {
-    fprintf(stderr, "ERROR\n");
+    BOOST_LOG_TRIVIAL(fatal) << "SERVER: Incorrect record size";
+    exit(-1);
   }
   else {
     unsigned int elapsed_msecs = timer.current_time()/1000;
-    fprintf(stderr, "POST_APPEND %d:%d\n",
-	    *(const unsigned int *)data,
-	    elapsed_msecs - *(const unsigned int *)((char *)data + 4));
+    BOOST_LOG_TRIVIAL(info) 
+      <<  "SERVER: POST_APPEND "
+      << *(const unsigned int *)data << " "
+      << (elapsed_msecs - *(const unsigned int *)((char *)data + 4));
   }
   __sync_synchronize();
 }
@@ -40,13 +45,15 @@ void trace_post_append(void *data, const int size)
 void trace_send_entry(void *data, const int size)
 {
   if(size != 8) {
-    fprintf(stderr, "ERROR\n");
+    BOOST_LOG_TRIVIAL(fatal) << "SERVER: Incorrect record size";
+    exit(-1);
   }
   else {
     unsigned int elapsed_msecs = timer.current_time()/1000;
-    fprintf(stderr, "SEND_ENTRY %d:%d\n",
-	    *(const unsigned int *)data,
-	    elapsed_msecs - *(const unsigned int *)((char *)data + 4));
+    BOOST_LOG_TRIVIAL(info) 
+      << "SERVER: SEND_ENTRY "
+      << *(const unsigned int *)data << " "
+      << (elapsed_msecs - *(const unsigned int *)((char *)data + 4));
   }
   __sync_synchronize();
 }
@@ -54,13 +61,15 @@ void trace_send_entry(void *data, const int size)
 void trace_recv_entry(void *data, const int size)
 {
   if(size != 8) {
-    fprintf(stderr, "ERROR\n");
+    BOOST_LOG_TRIVIAL(fatal) << "SERVER: Incorrect record size";
+    exit(-1);
   }
   else {
     unsigned int elapsed_msecs = timer.current_time()/1000;
-    fprintf(stderr, "RECV_ENTRY %d:%d\n",
-	    *(const unsigned int *)data,
-	    elapsed_msecs - *(const unsigned int *)((char *)data + 4));
+    BOOST_LOG_TRIVIAL(info) 
+      << "SERVER: RECV_ENTRY "
+      << *(const unsigned int *)data << " "
+      <<  (elapsed_msecs - *(const unsigned int *)((char *)data + 4));
   }
   __sync_synchronize();
 }
@@ -68,13 +77,15 @@ void trace_recv_entry(void *data, const int size)
 void cyclone_cb(void *user_arg, const unsigned char *data, const int len)
 {
   if(len != 8) {
-    fprintf(stderr, "ERROR\n");
+    BOOST_LOG_TRIVIAL(fatal) << "CLIENT: Incorrect record size";
+    exit(-1);
   }
   else {
     unsigned int elapsed_msecs = timer.current_time()/1000;
-    fprintf(stderr, "APPLY %d:%d\n",
-	    *(const unsigned int *)data,
-	    elapsed_msecs - *(const unsigned int *)(data + 4));
+    BOOST_LOG_TRIVIAL(info)
+      << "CLIENT: APPLY "
+      << *(const unsigned int *)data  << " "
+      << (elapsed_msecs - *(const unsigned int *)(data + 4));
   }
   __sync_synchronize();
 }
@@ -109,14 +120,16 @@ int main(int argc, char *argv[])
     free(cookie);
     unsigned int elapsed_msecs = timer.current_time()/1000;
     if(result == 1) {
-      fprintf(stderr, "LOG %d:%d\n",
-	      *(const unsigned int *)entry,
-	      elapsed_msecs - *(const unsigned int *)(entry + 4));
+      BOOST_LOG_TRIVIAL(info) 
+	<< "CLIENT: ACCEPT "
+	<< *(const unsigned int *)entry << " "
+	<< (elapsed_msecs - *(const unsigned int *)(entry + 4));
     }
     else {
-      fprintf(stderr, "REJECT %d:%d\n",
-	      *(const unsigned int *)entry,
-	      elapsed_msecs - *(const unsigned int *)(entry + 4));
+      BOOST_LOG_TRIVIAL(info)
+	<< "CLIENT: REJECT "
+	<< *(const unsigned int *)entry << " "
+	<< (elapsed_msecs - *(const unsigned int *)(entry + 4));
     }
   }
 }
