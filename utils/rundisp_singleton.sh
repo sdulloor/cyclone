d6734e1a000fa3317f56503262fbab2bc49475fd
Amitabha Roy <amitabha.roy@intel.com>
2015-11-10 11:03:48 -0800

    More tests.

---
+++ utils/rundisp_singleton.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -f /mnt/tmpfs/raftdata*
+rm -f /mnt/tmpfs/dispdata*
+echo "starting servers"
+cd test1
+./dispatch_test_server &> output &
+echo $!
+sleep 3
+echo "starting clients"
+cd ../test1
+./dispatch_test_client 0 &> output_client &
+echo $!
