9954790f8434e0b5362436a857f57ca2396171d3
Amitabha Roy <amitabha.roy@intel.com>
2015-11-16 23:51:14 -0800

    Update test script.

--- utils/runtest_sal_cluster.sh
+++ utils/runtest_sal_cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 GROUP='sal-sdv[1-4]'
 BASE_DIR=/data/devel/cyclone
+CLIENT_SLEEP=100
 clush -w ${GROUP} cp $BASE_DIR/cyclone.git/test/config_sal.ini $BASE_DIR/cyclone_test.ini
 clush -w ${GROUP} cp $BASE_DIR/cyclone.git/test/rbtree_map_server $BASE_DIR
 clush -w ${GROUP} cp $BASE_DIR/cyclone.git/test/rbtree_map_driver $BASE_DIR
@@ -18,7 +19,7 @@ clush -w sal-sdv${i} "echo '$CHANGE_DIR' >> $BASE_DIR/server_cmd.sh"
 clush -w sal-sdv${i} "echo '$SET_LDPATH' >> $BASE_DIR/server_cmd.sh"
 clush -w sal-sdv${i} "echo '$CMD' >> $BASE_DIR/server_cmd.sh"
 clush -w sal-sdv${i} chmod u+x $BASE_DIR/server_cmd.sh
-CMD="./rbtree_map_driver $id &> output_client &"
+CMD="./rbtree_map_driver $id $CLIENT_SLEEP &> output_client &"
 clush -w sal-sdv${i} "echo '$INTERP' > $BASE_DIR/client_cmd.sh"
 clush -w sal-sdv${i} "echo '$CHANGE_DIR' >> $BASE_DIR/client_cmd.sh"
 clush -w sal-sdv${i} "echo '$SET_LDPATH' >> $BASE_DIR/client_cmd.sh"
