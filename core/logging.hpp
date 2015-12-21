cf73b9d4ce9dba21f4379ff1e16d012d31032cfd
root <root@arch-h2.dp.intel.com>
2015-12-20 19:39:39 -0800

    Add missing file.

---
+++ core/logging.hpp
@@ -0,0 +1,46 @@
+#ifndef _LOGGING_
+#define _LOGGING_
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<boost/thread.hpp>
+enum log_state {
+  fatal = 0,
+  error = 1,
+  warning = 2,
+  info = 3,
+  total = 4
+};
+
+static const char *log_headers[total] = 
+  {"FATAL", 
+   "ERROR",
+   "WARNING",
+   "INFO"};
+
+class BOOST_LOG_TRIVIAL {
+  enum log_state level;
+public:
+  std::stringstream state;
+  BOOST_LOG_TRIVIAL(enum log_state level_in)
+    :level(level_in)
+  {
+  }
+  template<typename T>
+  BOOST_LOG_TRIVIAL& operator<< (T value)
+  {
+    state << value;
+    return *this;
+  }
+  
+  ~BOOST_LOG_TRIVIAL()
+  {
+    std::stringstream final;
+    final << "<" << log_headers[level] << " ";
+    final << boost::this_thread::get_id() << "> ";
+    final << state.str() << std::endl;
+    std::cerr << final.str();
+  }
+};
+
+#endif
