68f6d5509caf2fea4163d9d62c6c0e7613f41d7a
U-AMR\aroy1 <aroy1@aroy1-MOBL.amr.corp.intel.com>
2016-02-25 19:10:20 +0530

    Add in basic fault tolerance to coordinator.

--- test/rbtree_map_coordinator.cpp
+++ test/rbtree_map_coordinator.cpp
@@ -29,10 +29,172 @@ TOID(char) nvheap_setup(TOID(char) recovered,
 }
 
 
+int leader_callback_recovery(const unsigned char *data,
+			     const int len,
+			     unsigned char **follower_data,
+			     int *follower_data_size,
+			     void **return_value,
+			     int *ctr_recovery,
+			     int tx_status)
+{
+  rbtree_tx_t * tx = (rbtree_tx_t *)data;
+  costat *rep;
+  rep = (costat *)malloc(sizeof(costat));
+  *follower_data = (unsigned char *)rep;
+  *follower_data_size = sizeof(costat);
+  rep->tx_status  = 1;
+  struct kv *info;
+  struct k* del_info;
+
+  tx_client_response *client_resp;
+  *return_value = malloc(sizeof(tx_client_response));
+  client_resp = (tx_client_response *)*return_value;
+  client_resp->tx_status = tx_status;
+
+  if(tx_status == 0) {
+    for(int i=0;i<quorums;i++) {
+      ctr[i] = ctr_recovery[i];
+    }
+    goto cleanup;
+  }
+  
+  // Acquire locks
+  for(int i=0;i<tx->num_locks;i++) {
+    info = locks_list(tx, i);
+    int partition = info->key % quorums;
+    struct proposal req;
+    struct proposal *resp;
+    req.fn = FN_LOCK;
+    req.kv_data = *info;
+    req.src       = clients - 1;
+    req.order     = 0;
+
+    bool make_call = (ctr_recovery[partition] <= ctr[partition]);
+    if(make_call) {
+      int sz = make_rpc(quorum_handles[partition],
+			&req,
+			sizeof(struct proposal),
+			(void **)&resp,
+			ctr[partition],
+			0);
+    }
+    ctr[partition]++;
+
+    if(make_call && resp->code != req.kv_data.value) {
+      // Cleanup and fail
+      client_resp->tx_status = 0;
+      tx->num_locks = i;
+      break;
+    }
+    info->value = resp->code + 1;
+  }
+
+  if(client_resp->tx_status == 0) {
+    goto cleanup;
+  }
+  
+  for(int i=0;i<tx->num_versions;i++) {
+    info = versions_list(tx, i);
+    int partition = info->key % quorums;
+    struct proposal req;
+    struct proposal *resp;
+    req.fn = FN_GET_VERSION;
+    req.k_data.key = info->key;
+    req.src       = clients - 1;
+    req.order     = 0;
+    bool make_call = (ctr_recovery[partition] <= ctr[partition]);
+
+    if(make_call) {
+      int sz = make_rpc(quorum_handles[partition],
+			&req,
+			sizeof(struct proposal),
+			(void **)&resp,
+			ctr[partition],
+			0);
+
+    }
+    ctr[partition]++;
+    if(make_call && resp->code != req.kv_data.value) {
+      // Cleanup and fail
+      client_resp->tx_status = 0;
+      break;
+    }
+  }
+
+  if(client_resp->tx_status == 0) {
+    goto cleanup;
+  }
+  
+  for(int i=0;i<tx->num_inserts;i++) {
+    info = inserts_list(tx, i);
+    int partition = info->key % quorums;
+    struct proposal req;
+    struct proposal *resp;
+    req.fn = FN_INSERT;
+    req.kv_data = *info;
+    req.src       = clients - 1;
+    req.order     = 0;
+
+    bool make_call = (ctr_recovery[partition] <= ctr[partition]);
+    if(make_call) {
+      int sz = make_rpc(quorum_handles[partition],
+			&req,
+			sizeof(struct proposal),
+			(void **)&resp,
+			ctr[partition],
+			0);
+    }
+    ctr[partition]++;
+  }
+
+  for(int i=0;i<tx->num_deletes;i++) {
+    del_info = deletes_list(tx, i);
+    int partition = del_info->key % quorums;
+    struct proposal req;
+    struct proposal *resp;
+    req.fn = FN_DELETE;
+    req.kv_data = *info;
+    req.src       = clients - 1;
+    req.order     = 0;
+    bool make_call = (ctr_recovery[partition] <= ctr[partition]);
+    if(make_call) {
+      int sz = make_rpc(quorum_handles[partition],
+			&req,
+			sizeof(struct proposal),
+			(void **)&resp,
+			ctr[partition],
+			0);
+    }
+    ctr[partition]++;
+  }
+
+ cleanup:
+  
+  for(int i=0;i<tx->num_locks;i++) {
+    info = locks_list(tx, i);
+    int partition = info->key % quorums;
+    struct proposal req;
+    struct proposal *resp;
+    req.fn = FN_UNLOCK;
+    req.kv_data = *info;
+    req.src       = clients - 1;
+    req.order     = 0;
+    int sz = make_rpc(quorum_handles[partition],
+		  &req,
+		  sizeof(struct proposal),
+		  (void **)&resp,
+		  ctr[partition],
+		  0);
+    ctr[partition]++;
+  }
+
+  return sizeof(tx_client_response);
+}
+
 int leader_callback(const unsigned char *data,
 		    const int len,
 		    unsigned char **follower_data,
-		    int * follower_data_size, 
+		    int *follower_data_size, 
 		    void **return_value)
 {
   rbtree_tx_t * tx = (rbtree_tx_t *)data;
@@ -66,6 +228,24 @@ int leader_callback(const unsigned char *data,
 		      (void **)&resp,
 		      ctr[partition],
 		      0);
+    if(sz == RPC_EOLD) {
+      int *recovery_ctr = new int[quorums];
+      for(int i=0;i<quorums;i++) {
+	recover_ctr[i] = get_last_txid(quorum_handles[i]) + 1;
+      }
+      free(rep);
+      free(*return_value);
+      int recovery_ret_value =
+	leader_callback_recovery(data,
+				 len,
+				 follower_data,
+				 follower_data_size,
+				 return_value,
+				 recovery_ctr,
+				 1); // TBD: fix tx status
+      delete recover_ctr;
+      return recovery_ret_value;
+    }
 
     ctr[partition]++;
     if(resp->code != req.kv_data.value) {
