f6afb230e5abb60581642b98f81a555e4aa22cd1
root <root@arch-h2.dp.intel.com>
2016-03-25 20:27:25 -0700

    Aggressive failure detection :)

--- core/timeouts.hpp
+++ core/timeouts.hpp
@@ -3,10 +3,10 @@
 
 // Server side timeouts -- usecs
 const int PERIODICITY                = 10; 
-const int RAFT_ELECTION_TIMEOUT      = 1000000; 
+const int RAFT_ELECTION_TIMEOUT      = 50000; 
 const int RAFT_REQUEST_TIMEOUT       = 2000; // warning: this should be high enough !
 
 // Client side timeouts
-static const int timeout_msec  = 10000; // Client - failure detect
+static const int timeout_msec  = 100; // Client - failure detect
 
 #endif
