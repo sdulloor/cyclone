fabb530d8dd9e57b77c809c8ec1fe335b3dc02fa
root <root@arch-h2.dp.intel.com>
2016-05-31 17:30:24 -0700

    Updated sweep script.

--- utils-global/rd_wr_sweep.sh
+++ utils-global/rd_wr_sweep.sh
@@ -1,104 +1,92 @@
 #!/bin/bash
 ROOT=$PWD
 echo "running in $ROOT"
-rm -rf sweep_results*
-rm -rf progress
-KEYS=1000000
-MAX_CLIENTS=36
-MAX_RUN_CLIENTS=28
-for q in `seq 1 1 4`
-do
-    ${ROOT}/../killtest_arch_cluster.sh
-    mkdir sweep_results_q_$q
-    cd sweep_results_q_$q
-    mkdir preload
-    cd preload
-    echo '[meta]' > example.ini
-    echo "quorums=$q" >> example.ini
-    echo "clients=$MAX_CLIENTS" >> example.ini
-    cat ${ROOT}/example_base.ini >> example.ini
-    cp ${ROOT}/cluster.ini .
-    python ${ROOT}/../cyclone.git/utils-global/config_generator.py cluster.ini example.ini test_output
-    clush -w arch-h[2-24/2] rm -f exec_servers.sh
-    clush -w arch-h[2-24/2] rm -f exec_coord.sh
-    clush -w arch-h[2-24/2] rm -f /root/cyclone/server_log
-    ${ROOT}/../cyclone.git/utils-global/deploy_configs.sh test_output /root/cyclone arch-h[2-24/2]
-    ${ROOT}/../cyclone.git/utils-global/deploy_services.sh test_output /root/cyclone arch-h[2-24/2]
-    ${ROOT}/../cyclone.git/utils-global/deploy_driver.sh /root/cyclone arch-h[2-24/2]
-    clush -w arch-h[2-24/2] "echo export RBT_KEYS=$KEYS > /tmp/x"
-    clush -w arch-h[2-24/2] "echo export RBT_FRAC_READ=$rd_frac_float >> /tmp/x"
-    clush -w arch-h[2-24/2] "cat /root/cyclone/launch_preload >> /tmp/x"
-    clush -w arch-h[2-24/2] "mv /tmp/x /root/cyclone/launch_preload"
-    echo "Preloading $q" >> ${ROOT}/progress
-    clush -w arch-h[2-24/2] /root/cyclone/exec_preload.sh
-    mc=2
-    last_c=$(($MAX_CLIENTS - 1))
-    for mx in `seq 0 1 $last_c`
+export RBT_KEYS=1000
+export RBT_FRAC_READ=0.5  
+MAX_CLIENTS=200
+MAX_RUN_CLIENTS=1
+now=$(date +"%T-%m_%d_%Y")
+configs_dir=test_output
+deploy_dir=/root
+logs_dir="logs"
+
+rm -rf ${now}
+rm -rf ${ROOT}/progress
+
+mkdir ${now}
+cd ${now}
+
+copy_logs () {
+    target=$1
+    for i in ${configs_dir}/* 
     do
-	scp arch-h${mc}:/root/cyclone/client_log${mx} client_log${mx}_${mc}
-	mc=$(($mc + 2))
-	if [ "$mc" -gt "24" ]; then
-	    mc=2
+	if [ -d "$i" ] ; then
+	    node=$(basename $i)
+	    ip=`cat ${i}/ip_address`
+	    if [ -s "$i/launch_clients" ] ; then
+		mkdir ${target}/${node}
+		scp ${ip}:${deploy_dir}/${node}/client_log* ${target}/${node}
+	    fi
+	    if [ -s "$i/launch_servers" ] ; then
+		mkdir -p ${target}/${node}
+		scp ${ip}:${deploy_dir}/${node}/server_log ${target}/${node}
+	    fi
 	fi
     done
-    echo "Complete preload" >> ${ROOT}/progress
-    cd ..
-    for rd_frac in `seq 10 20 90`
+}
+
+shutdown () {
+    ${ROOT}/../cyclone.git/utils-global/deploy_shutdown.sh ${configs_dir} ${deploy_dir}
+}
+
+mkconfig () {
+    cp ${ROOT}/cluster.ini .
+    cp ${ROOT}/counter_launcher.py .
+    cp ${ROOT}/example.ini .
+    python ${ROOT}/../cyclone.git/utils-global/config_generator.py cluster.ini example.ini counter_launcher.py ${configs_dir}
+    ${ROOT}/../cyclone.git/utils-global/deploy_shutdown.sh ${configs_dir} ${deploy_dir}
+    ${ROOT}/../cyclone.git/utils-global/deploy_configs.sh ${configs_dir} ${deploy_dir}
+}
+
+preload () {    
+    echo "Preloading $q" >> ${ROOT}/progress
+    ${ROOT}/../cyclone.git/utils-global/deploy_services.sh ${configs_dir} ${deploy_dir}
+    ${ROOT}/../cyclone.git/utils-global/deploy_preload.sh ${configs_dir} ${deploy_dir}
+    echo "Completed preload" >> ${ROOT}/progress
+}
+
+runtest () {
+    ${ROOT}/../cyclone.git/utils-global/deploy_clients.sh ${configs_dir} ${deploy_dir}
+    echo "Running test .. sleeping 2 mins." >> ${ROOT}/progress
+    sleep 120
+    shutdown
+    echo "Completed test" >> ${ROOT}/progress
+    mkdir logs
+    copy_logs "${logs_dir}"
+}
+
+
+
+for q in `seq 1 1 1`
+do
+    quorums_plus_one=$(($q + 1)) # co-ord takes last quorum
+    mkdir q_$q
+    cd q_$q
+    for c in `seq 1 1 $MAX_RUN_CLIENTS` 
     do
-	rd_frac_float=`echo $rd_frac/100 | bc -l`
-	mkdir rd_frac_${rd_frac}
-	cd rd_frac_${rd_frac}
-	for c in `seq 1 1 $MAX_RUN_CLIENTS`
-	do
-	    echo "Exec $rd_frac $q $c" >> ${ROOT}/progress
-	    echo "quorum=$q clients=$c"
-	    mclast=$(($c + $c))
-	    if [ "$mclast" -gt "24" ]; then
-		mclast=24
-	    fi
-	    echo "mclast=$mclast"
-	    config_clients=$(($c + 1)) # account for coordinator
-	    mkdir c_$c
-	    cd c_$c
-	    echo "Running test" >> ${ROOT}/progress
-	    echo '[meta]' > example.ini
-	    echo "quorums=$q" >> example.ini
-	    echo "clients=$config_clients" >> example.ini
-	    cat ${ROOT}/example_base.ini >> example.ini
-	    cp ${ROOT}/cluster.ini .
-	    python ${ROOT}/../cyclone.git/utils-global/config_generator.py cluster.ini example.ini test_output
-	    ${ROOT}/../cyclone.git/utils-global/deploy_configs.sh test_output /root/cyclone arch-h[2-24/2]
-	    ${ROOT}/../cyclone.git/utils-global/deploy_driver.sh /root/cyclone arch-h[2-24/2]
-	    clush -w arch-h[2-${mclast}/2] rm -f '/root/cyclone/client_log*'
-	    clush -w arch-h[2-${mclast}/2] "echo export RBT_KEYS=$KEYS > /tmp/x"
-	    clush -w arch-h[2-${mclast}/2] "echo export RBT_FRAC_READ=$rd_frac_float >> /tmp/x"
-	    clush -w arch-h[2-${mclast}/2] "cat /root/cyclone/launch_client >> /tmp/x"
-	    clush -w arch-h[2-${mclast}/2] "mv /tmp/x /root/cyclone/launch_client"
-	    clush -w arch-h[2-${mclast}/2] /root/cyclone/exec_client.sh
-	    echo "Sleeping for 2 min while test runs"
-	    sleep 120 # run for 2 min
-	    clush -w arch-h[2-${mclast}/2] killall -9 counter_driver
-	    mc=2
-	    last_c=$(($c - 1))
-	    for mx in `seq 0 1 $last_c`
-	    do
-		scp arch-h${mc}:/root/cyclone/client_log${mx} client_log${mx}_${mc}
-		mc=$(($mc + 2))
-		if [ "$mc" -gt "24" ]; then
-		    mc=2
-		fi
-	    done
-	    echo "Complete $rd_frac $q $c" >> ${ROOT}/progress
-	    cd ..
-	done
+	mkdir c_$c
+	cd c_$c
+	echo "q=$q c=$c" >> ${ROOT}/progress
+	echo '[meta]' > ${ROOT}/example.ini
+	echo "quorums=$quorums_plus_one" >> ${ROOT}/example.ini
+	clients_plus_one=$(($c + 1)) # co-ord takes last client
+	echo "clients=$clients_plus_one" >> ${ROOT}/example.ini
+	cat ${ROOT}/example_base.ini >> ${ROOT}/example.ini
+	mkconfig
+	preload
+	runtest
 	cd ..
     done
-    cd preload
-    for m in `seq 2 2 24`
-    do
-	scp arch-h${m}:/root/cyclone/server_log server_log_${m}
-    done
-    cd ..
     cd ..
 done
-${ROOT}/../killtest_arch_cluster
+cd ${ROOT}
