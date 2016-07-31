a57bf38b3afa88e6b0ac1a1bd466ae7aeccdf597
root <root@arch-h2.jf.intel.com>
2016-07-31 13:23:53 -0700

    Add leading mfence.

--- core/clwb_sim.hpp
+++ core/clwb_sim.hpp
@@ -7,6 +7,7 @@ static const int CLFLUSHOPT_WINDOW = 10;
 static void clflush(void *ptr, int size)
 {
   char *x = (char *)ptr;
+  asm volatile("mfence");
   while(size > 64*CLFLUSHOPT_WINDOW) {
     asm volatile("clflush %0"::"m"(*x));
     x    += 64*CLFLUSHOPT_WINDOW;
