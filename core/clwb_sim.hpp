b1e6bbcfd781bf35e6f122dd265a06cf16575357
root <root@arch-h2.jf.intel.com>
2016-07-31 13:09:49 -0700

    add missing file.

---
+++ core/clwb_sim.hpp
@@ -0,0 +1,19 @@
+#ifndef _CLWBOPTSIM_
+#define _CLWBOPTSIM_
+
+
+static const int CLFLUSHOPT_WINDOW = 10;
+
+static void clflush(void *ptr, int size)
+{
+  char *x = (char *)ptr;
+  while(size > 64*CLFLUSHOPT_WINDOW) {
+    asm volatile("clflush %0"::"m"(*x));
+    x    += 64*CLFLUSHOPT_WINDOW;
+    size -= 64*CLFLUSHOPT_WINDOW;
+  }
+  asm volatile("clflush %0;mfence"::"m"(*x));
+}
+
+
+#endif
