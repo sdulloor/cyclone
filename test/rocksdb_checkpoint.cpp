0081ea5c8555ab7c2fe99a4a6cca7385232b23bb
root <root@arch-h2.jf.intel.com>
2017-04-13 16:58:48 -0700

    Fix.

--- test/rocksdb_checkpoint.cpp
+++ test/rocksdb_checkpoint.cpp
@@ -59,14 +59,13 @@ void opendb(){
   env->SetBackgroundThreads(2, rocksdb::Env::LOW);
   env->SetBackgroundThreads(1, rocksdb::Env::HIGH);
   options.env = env;
-  options.PrepareForBulkLoad();
   options.write_buffer_size = 1024 * 1024 * 256;
   options.target_file_size_base = 1024 * 1024 * 512;
   options.max_background_compactions = 2;
   options.max_background_flushes = 1;
   options.max_write_buffer_number = 3;
   //options.min_write_buffer_number_to_merge = max(num_threads/2, 1);
-  options.compaction_style = rocksdb::kCompactionStyleNone;
+  //options.compaction_style = rocksdb::kCompactionStyleNone;
   //options.memtable_factory.reset(new rocksdb::VectorRepFactory(1000));
   rocksdb::Status s = rocksdb::DB::Open(options, preload_dir, &db);
   if (!s.ok()){
