716115ae68b3858aa0c4717d6a06da98983a9f09
U-AMR\aroy1 <aroy1@aroy1-MOBL.amr.corp.intel.com>
2017-08-24 16:41:03 -0700

    Cleanup utils-arch-cluster.

--- utils-arch-cluster/killtest_arch_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GROUP='arch-h[2-30/2].dp.intel.com'
-
-clush -w ${GROUP} killall -9 counter_server
-clush -w ${GROUP} killall -9 counter_coordinator
-clush -w ${GROUP} killall -9 counter_coordinator_driver
-clush -w ${GROUP} killall -9 counter_driver
-clush -w ${GROUP} killall -9 counter_loader
-
